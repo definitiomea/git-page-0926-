8312bbdcce22163631e122a7c43d689a4c1f098e
Johnny_P <shinezl39@gmail.com>
2022-09-26 14:49:28 +0900

    updated

--- 0926Test.js
+++ 0926Test.js
@@ -2,15 +2,18 @@ let tablefield = document.getElementById("tablefield");
 
 function table() {
     let tableNum = prompt("원하는 단을 입력하세요");
+    let content = "";
     if(tableNum >= 2 && tableNum <= 9) {
-        document.write(`<h1>${tableNum}단</h1>`);
+        content += `<h1>${tableNum}단</h1>`;
         for(let i = tableNum; i <= tableNum; i++) {
             for(let j = 1; j <= 9; j++) {
-                document.write(`<p>${tableNum} X ${j} = ${tableNum * j}</p>`);
+                content += `<p>${tableNum} X ${j} = ${tableNum * j}</p>`;
             }
         }
+        tablefield.innerHTML = content;
     }
     else {
         alert("구구단에 들어가는 수가 아닙니다.");
+        tablefield.innerHTML = "";
     }
 }
